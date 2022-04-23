959d6acc4263ba876fab3f1a192fad6ea06d641e
daxab <93872444+daxab@users.noreply.github.com>
2022-04-23 18:01:25 +0300

    занятие

---
+++ QuizLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizLibrary
+{
+    public class Class1
+    {
+       public void Show()
+       {
+            System.Windows.Forms.MessageBox.Show("df");
+       }
+        public void r()
+        { }
+    }
+}
---
+++ QuizViewer/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuizLibrary;
+
+
+namespace SimpleQuizer.Viewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Class1 c = new Class1();
+            c.r();
+        }
+    }
+}
